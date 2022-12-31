416f8f90d7e2f34c8193a2e0eb33cc8d9910924a
HV220 <diagenin@gmail.com>
2022-12-31 18:54:31 +0300

    init yii

---
+++ app/environments/prod/common/config/main-local.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'components' => [
+        'db' => [
+            'class' => \yii\db\Connection::class,
+            'dsn' => 'pgsql:host=' . $_ENV['DB_HOST'] . 'port=' . $_ENV['DB_PORT'] . ';dbname=' . $_ENV['DB_NAME'],
+            'username' => $_ENV['DB_USER'],
+            'password' => $_ENV['DB_PASSWORD'],
+        ],
+    ],
+];
